78cdc8b1ce5daa24b516c1da3f4881b48287822f
TempleLo <templelo5408@gmail.com>
2021-08-30 23:21:02 +0800

    Set to return -1 in main if GLFW window creation fails.

--- LTM_CPPOpenGLAPI.cpp
+++ LTM_CPPOpenGLAPI.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]){
     if(!window){
         std::cout << "Failed to create GLFW window" << "\n";
         glfwTerminate();
-        return 1;
+        return -1;
     }
 
     glfwMakeContextCurrent(window);
