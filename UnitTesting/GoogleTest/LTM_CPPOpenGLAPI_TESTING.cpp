1389e70a805ec8d9b9c1adcbade0edf2aa6bead4
TempleLo <templelo5408@gmail.com>
2021-09-01 00:22:30 +0800

    Configured GoogleTest.

---
+++ UnitTesting/GoogleTest/LTM_CPPOpenGLAPI_TESTING.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <iostream>
+
+// Demonstrate some basic assertions.
+TEST(HelloTest, BasicAssertions) {
+  // Expect two strings not to be equal.
+  EXPECT_STRNE("hello", "world");
+  // Expect equality.
+  EXPECT_EQ(7 * 6, 42);
+}
+
+int test(int argc, char** argv) {
+	printf("Running main() from %s\n", __FILE__);
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+GTEST_API_ int main(int argc, char** argv) {
+	test(argc, argv);
+	std::cout << "Press any key to end." << std::endl;
+	std::cin.get();
+}
